a20689fa8d32f26d502e4a4c90ed1f5c170ce298
I m r a n <imurokks@gmail.com>
2023-12-02 19:15:40 +0530

    Autoloader added

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,7 @@ import java.util.List;
 @RequestMapping(path="api/v1/student")
 public class StudentController {
     private final StudentService studentService;
-
+@Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
